98be7936b163793312485d23bf7f8779c639feb6
Ethan Richardson <ethanx94@gmail.com>
2019-03-03 22:01:52 -0500

    offload to github gists

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
+let activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   pathPrefix: 'FGR',
   siteMetadata: {
@@ -18,6 +26,24 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'queries',
+        path: `${__dirname}/src/queries/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        headers: {
+          Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        typeName: "GitHub",
+        fieldName: "github",
+        url: "https://api.github.com/graphql",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,110 +1,41 @@
 import React from 'react';
-import Person from '../components/person';
+import { graphql } from "gatsby"
 
+import Person from '../components/person';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => {
+  const { github: { viewer: { gist: { files } } } } = data;
+  const newData = JSON.parse(files[0].text);
+  return (
   <Layout>
     <SEO title="Family Group Record" />
     <table className="table">
-      {Object.keys(data).map(
+      {Object.keys(newData).map(
         (d, idx) =>
-        !Array.isArray(data[d]) 
-          ? <Person data={data[d]} type={d} key={idx} />
-          : data[d].map((child, childIdx) =>
+        !Array.isArray(newData[d]) 
+          ? <Person data={newData[d]} type={d} key={idx} />
+          : newData[d].map((child, childIdx) =>
               <Person key={childIdx} data={child} type={'children'} />)
         )}
       </table>
   </Layout>
-);
+)};
 
-const data = {
-  "husband": {
-      "givenName": "Chamerlain, Sr",
-      "lastName": "Hutson",
-      "born": {
-        "date":"15 Sep 1779",
-        "place": "NC",
-      },
-      "died": {
-        "date":"03 Oct 1858",
-        "place": "Benton, IL",
-      },
-      "buried": {
-        "date":"",
-        "place": "Browning Hill Cem.",
-      },
-      "married": {
-        "date":"17 May 1802",
-        "place": "Logan, KY",
-      },
-      "notes": "1850 Frlo IL next to Wm A King + Siblings near Burker + Bf. Ramson"
-  },
-  "wife": {
-      "givenName": "Anice/Anis/Annis",
-      "maidenName": "Lee",
-      "born": {
-        "date":"1779/1783",
-        "place": "NC",
-      },
-      "died": {
-        "date":"1866",
-        "place": "",
-      },
-      "buried": {
-        "date":"",
-        "place": "",
-      },
-      "married": {
-        "date":"",
-        "place": "",
-      },
-      "notes": ""
-  },
-  "children": [{
-      "sex": "F",
-      "givenName": "Elizabeth",
-      "lastName": "Hutson",
-      "born": {
-        "date":"c 1803/04",
-        "place": "TN or IL ⊕ John Sandusky",
-      },
-      "died": {
-        "date":"c 1805",
-        "place": "",
-      },
-      "buried": {
-        "date":"",
-        "place": "",
-      },
-      "married": {
-        "date":"c 1820/21",
-        "place": "",
-      },
-      "notes": ""
-  },{
-    "sex": "M",
-    "givenName": "Innes",
-    "lastName": "Mcclerrin",
-    "born": {
-      "date":"May 1834",
-      "place": "KY",
-    },
-    "died": {
-      "date":"",
-      "place": "",
-    },
-    "buried": {
-      "date":"",
-      "place": "",
-    },
-    "married": {
-      "date":"1862",
-      "place": "",
-    },
-    "notes": ""
-}]
-};
+export const query = graphql`
+  query IndexPageQuery {
+    github {
+      viewer { 
+        gist (name: "e48b904900f674cf529876b39af26677") {
+          files {
+            name
+            text
+          }
+        } 
+      }
+    }
+  }
+`
 
 export default IndexPage
